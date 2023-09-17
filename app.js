204610f474285f6763a8c4768678caa266e19a17
AidenW-Git <nicho20006@gmail.com>
2023-09-17 14:38:04 -0400

    Huge

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const WebSocket = require('ws');
-const http = require('http');
-const express = require('express');
 
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ port: 3000 });
 
-// Initialize scores
 let leftScore = 0;
 let rightScore = 0;
-
-app.use(express.static(__dirname)); // Serve static files (e.g., index.html, styles.css)
 
 wss.on('connection', (ws) => {
     console.log('WebSocket connection established.');
@@ -19,29 +12,17 @@ wss.on('connection', (ws) => {
         console.log(`Received message: ${message}`);
 
         // Process WebSocket messages here
-        switch (message) {
-            case 'IncrementLeft':
-                leftScore++;
-                break;
-            case 'DecrementLeft':
-                leftScore--;
-                break;
-            case 'IncrementRight':
-                rightScore++;
-                break;
-            case 'DecrementRight':
-                rightScore--;
-                break;
-            case 'Reset':
-                leftScore = 0;
-                rightScore = 0;
-                break;
-            default:
-                console.log('Unknown message:', message);
-                break;
+        if (message === 'IncrementLeft') {
+            leftScore++;
+        } else if (message === 'DecrementLeft') {
+            leftScore--;
+        } else if (message === 'IncrementRight') {
+            rightScore++;
+        } else if (message === 'DecrementRight') {
+            rightScore--;
         }
 
-        // Broadcast updated scores to all connected clients
+        // Send updated scores to all connected clients
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify({ leftScore, rightScore }));
@@ -51,8 +32,4 @@ wss.on('connection', (ws) => {
 
     // Send initial scores to the newly connected client
     ws.send(JSON.stringify({ leftScore, rightScore }));
-});
-
-server.listen(3000, () => {
-    console.log('WebSocket server is running on port 3000.');
 });
