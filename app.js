3a9b978c7230640e6496fea6ddb30d3a8b0097d6
AidenW-Git <nicho20006@gmail.com>
2023-09-17 14:58:30 -0400

    Huge

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-// ...
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
-// Handle incoming WebSocket messages
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Serve static files (e.g., HTML, CSS)
+app.use(express.static(__dirname));
+
+// Initialize scores
+let leftScore = 0;
+let rightScore = 0;
+
+// WebSocket connection handling
 wss.on('connection', (ws) => {
     console.log('WebSocket connection established.');
 
     // Send initial scores to the newly connected client
     ws.send(JSON.stringify({ leftScore, rightScore }));
 
+    // Handle incoming WebSocket messages
     ws.on('message', (message) => {
         console.log(`Received message: ${message}`);
 
@@ -48,4 +62,8 @@ wss.on('connection', (ws) => {
     });
 });
 
-// ...
+// Start the server
+const PORT = process.env.PORT || 3000; // Use the provided port or default to 3000
+server.listen(PORT, () => {
+    console.log(`WebSocket server is running on port ${PORT}.`);
+});
