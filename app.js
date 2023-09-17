31c2149a5f9f6581ef32903f8646bbf35cffd8e1
AidenW-Git <nicho20006@gmail.com>
2023-09-17 15:03:36 -0400

    Huge

--- app.js
+++ app.js
@@ -25,40 +25,34 @@ wss.on('connection', (ws) => {
         console.log(`Received message: ${message}`);
 
         // Process WebSocket messages here
-        try {
-            const parsedMessage = JSON.parse(message);
-
-            switch (parsedMessage.action) {
-                case 'IncrementLeft':
-                    leftScore++;
-                    break;
-                case 'DecrementLeft':
-                    leftScore--;
-                    break;
-                case 'IncrementRight':
-                    rightScore++;
-                    break;
-                case 'DecrementRight':
-                    rightScore--;
-                    break;
-                case 'Reset':
-                    leftScore = 0;
-                    rightScore = 0;
-                    break;
-                default:
-                    console.log('Unknown action:', parsedMessage.action);
-                    break;
-            }
-
-            // Broadcast updated scores to all connected clients
-            wss.clients.forEach((client) => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ leftScore, rightScore }));
-                }
-            });
-        } catch (error) {
-            console.error('Error parsing message:', error);
+        switch (message) {
+            case 'IncrementLeft':
+                leftScore++;
+                break;
+            case 'DecrementLeft':
+                leftScore--;
+                break;
+            case 'IncrementRight':
+                rightScore++;
+                break;
+            case 'DecrementRight':
+                rightScore--;
+                break;
+            case 'Reset':
+                leftScore = 0;
+                rightScore = 0;
+                break;
+            default:
+                console.log('Unknown action:', message);
+                break;
         }
+
+        // Broadcast updated scores to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ leftScore, rightScore }));
+            }
+        });
     });
 });
 
