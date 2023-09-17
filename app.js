9eb5a6aac6b3b82b53fa381853a9eae97364eb72
AidenW-Git <nicho20006@gmail.com>
2023-09-17 14:31:40 -0400

    Huge

--- app.js
+++ app.js
@@ -6,13 +6,51 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// Initialize scores
+let leftScore = 0;
+let rightScore = 0;
+
+app.use(express.static(__dirname)); // Serve static files (e.g., index.html, styles.css)
+
 wss.on('connection', (ws) => {
     console.log('WebSocket connection established.');
 
     ws.on('message', (message) => {
         console.log(`Received message: ${message}`);
+
         // Process WebSocket messages here
+        switch (message) {
+            case 'IncrementLeft':
+                leftScore++;
+                break;
+            case 'DecrementLeft':
+                leftScore--;
+                break;
+            case 'IncrementRight':
+                rightScore++;
+                break;
+            case 'DecrementRight':
+                rightScore--;
+                break;
+            case 'Reset':
+                leftScore = 0;
+                rightScore = 0;
+                break;
+            default:
+                console.log('Unknown message:', message);
+                break;
+        }
+
+        // Broadcast updated scores to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ leftScore, rightScore }));
+            }
+        });
     });
+
+    // Send initial scores to the newly connected client
+    ws.send(JSON.stringify({ leftScore, rightScore }));
 });
 
 server.listen(3000, () => {
